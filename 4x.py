d38a78c35ecbdbb950f99a6497ede3d8f002c370
Sujoy Sarker <114843414+Sjsarker@users.noreply.github.com>
2023-06-27 14:11:55 +0600

    4x.py

---
+++ 4x.py
@@ -0,0 +1,55 @@
+#-------------------Main--------------------#
+import os,sys,time,random,string
+import json.platform
+#-------------------Import--------------------#
+try:
+	  import requests
+expect ImportError:
+         os.system('pip install requests')
+         import mechanize
+ except ImportError:
+ 	    os.system('pip install mechanize')
+         import bs4
+except ImportError:
+	     os.system('pip install bs4')
+#-------------------xxx--------------------#
+from concurrent.futures import ThreadPoolExecutor as xxx
+from bs4 import Beautifulsoup as sop
+
+#-------------------color--------------------#
+green='\0.33[1;32m'
+red= '\0.33[1;31m'
+yellow='\0.33[1;33m'
+blue='\0.33[1;34'
+white='\0.33[1;37m'
+
+#-------------------logo--------------------#
+def logo():
+	os.system('clear')
+	print(f' ' '{white}
+     ..######...######...######.
+    .##....##.##....##.##....##
+    .##.......##.......##......
+    ..######...######...######.
+     .......##.......##.......##
+     .##....##.##....##.##....##
+    ..######...######...######.
+----------------------------------------------------------
+{white} [{green}+{white}]  FACEBOOK : Sujoy Sarker
+{white} [{green}+{white}] GitHub : Sjsarker
+{white} [{green}+{white}]  QUALITY : TESTING....
+----------------------------------------------------------------------------------' ' ')
+
+#-------------------sss_main--------------------#
+def main_sss():
+	logo()
+	
+	
+	
+try:
+	main_sss()
+except: 
+     main_sss()
+	
+	
+
